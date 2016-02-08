04b254d1749a93ee3d1ac453999b75cf1f3e20dc
Daniel Morgan <danmorgz@gmail.com>
2016-02-08 15:35:26 +0000

    using rotate guesture. Update constraints rather than frame of view

--- functional-reactive-intuition/Project/RFP/ImperativeViewController.swift
+++ functional-reactive-intuition/Project/RFP/ImperativeViewController.swift
@@ -9,85 +9,98 @@
 import UIKit
 
 class ImperativeViewController: UIViewController, UIGestureRecognizerDelegate {
-
-  var panPresent = false
-  var pinchPresent = false
-  var gestureTimer: NSTimer?
-  var secondsLeft = 3
-  
-  @IBOutlet weak var draggableView: UIView!
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    let pan = UIPanGestureRecognizer(target: self, action: "handlePan:")
-    pan.delegate = self
-    let pinch = UIRotationGestureRecognizer(target: self, action: "handleRotate:")
-    pinch.delegate = self
-    self.draggableView.gestureRecognizers = [pan, pinch]
-  }
-  
-
-  func handlePan(panGesture: UIPanGestureRecognizer) {
-
-    //Handle our state
-    if panGesture.state == .Began && self.panPresent == false {
-      self.panPresent = true
-      self.checkIfBothGesturesPresent()
-    } else if panGesture.state == .Ended {
-      self.panPresent = false
-      self.stopTimerIfNeeded()
+    var panPresent = false
+    var pinchPresent = false
+    var gestureTimer: NSTimer?
+    var secondsLeft = 3
+    
+    @IBOutlet weak var draggableView: UIView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var centerXConstraint: NSLayoutConstraint! //For updating the position of the box when dragging
+    @IBOutlet weak var centerYConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let pan = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        pan.delegate = self
+        let rotate = UIRotationGestureRecognizer(target: self, action: "handleRotate:")
+        rotate.delegate = self
+        self.draggableView.gestureRecognizers = [pan, rotate]
     }
     
-    // Move the view
-    let translation = panGesture.translationInView(self.view)
-    panGesture.view!.center = CGPoint(x: panGesture.view!.center.x + translation.x, y: panGesture.view!.center.y + translation.y)
-    panGesture.setTranslation(CGPointZero, inView: self.view)
-    
-  }
-  
-  func handleRotate(rotationGesture: UIRotationGestureRecognizer) {
-    
-    //Handle our state
-    if rotationGesture.state == .Began && self.pinchPresent == false {
-        self.pinchPresent = true
-        self.checkIfBothGesturesPresent()
-    } else if rotationGesture.state == .Ended {
-        self.pinchPresent = false
-        self.stopTimerIfNeeded()
+    override func viewWillAppear(animated: Bool) {
+        self.setStatus("Status: Waiting for Rotate & Pan")
     }
     
-    // Move the view
-    rotationGesture.view!.transform = CGAffineTransformRotate(rotationGesture.view!.transform,rotationGesture.rotation)
-    rotationGesture.rotation = 0;
-}
-  
-  func checkIfBothGesturesPresent() {
-    if self.pinchPresent == true && self.panPresent == true && self.gestureTimer == nil {
-      self.secondsLeft = 3
-      self.gestureTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick:", userInfo: nil, repeats: true)
-      print("started")
+    func handlePan(panGesture: UIPanGestureRecognizer) {
+        
+        // Move the view
+        let translation = panGesture.translationInView(self.view)
+        self.centerXConstraint.constant += translation.x
+        self.centerYConstraint.constant += translation.y
+        
+        panGesture.setTranslation(CGPointZero, inView: self.view)
+        
+        //Handle our state
+        if panGesture.state == .Began && self.panPresent == false {
+            self.panPresent = true
+            self.checkIfBothGesturesPresent()
+        } else if panGesture.state == .Ended {
+            self.panPresent = false
+            self.stopTimerIfNeeded()
+        }
     }
-  }
-  
-  func stopTimerIfNeeded() {
-    if let gestureTimer = gestureTimer {
-      gestureTimer.invalidate()
-      self.gestureTimer = nil
-      print("completed")
+    
+    func handleRotate(rotationGesture: UIRotationGestureRecognizer) {
+        
+        // Move the view
+        rotationGesture.view!.transform = CGAffineTransformRotate(rotationGesture.view!.transform,rotationGesture.rotation)
+        rotationGesture.rotation = 0;
+        
+        //Handle our state
+        if rotationGesture.state == .Began && self.pinchPresent == false {
+            self.pinchPresent = true
+            self.checkIfBothGesturesPresent()
+        } else if rotationGesture.state == .Ended {
+            self.pinchPresent = false
+            self.stopTimerIfNeeded()
+        }
     }
-  }
-  
-  func tick(timer: NSTimer) {
-    if self.secondsLeft <= 0 {
-      self.stopTimerIfNeeded()
-      return
+    
+    func checkIfBothGesturesPresent() {
+        if self.pinchPresent == true && self.panPresent == true && self.gestureTimer == nil {
+            self.secondsLeft = 3
+            self.gestureTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick:", userInfo: nil, repeats: true)
+            self.setStatus("Started")
+        }
     }
-    self.secondsLeft--
-    print("tick")
-  }
-  
-  func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-    return true
-  }
+    
+    func stopTimerIfNeeded() {
+        if let gestureTimer = gestureTimer {
+            gestureTimer.invalidate()
+            self.gestureTimer = nil
+            self.setStatus("Completed")
+        }
+    }
+    
+    func tick(timer: NSTimer) {
+        if self.secondsLeft <= 0 {
+            self.stopTimerIfNeeded()
+            return
+        }
+        self.secondsLeft--
+        self.setStatus("Tick: \(self.secondsLeft)")
+    }
+    
+    func setStatus(statusString:String) {
+        
+        print(statusString)
+        self.statusLabel.text = statusString
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
     
 }

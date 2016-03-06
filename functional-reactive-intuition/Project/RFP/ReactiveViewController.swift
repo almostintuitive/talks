d31e82f2a4e66b172ead34c6be945fdf5d01b37e
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2016-03-06 11:47:18 +0100

    refactor: using map instread of flatMap where appropriate, initializing ReactiveGestureReactor with UIGestureRecognizers

--- functional-reactive-intuition/Project/RFP/ReactiveViewController.swift
+++ functional-reactive-intuition/Project/RFP/ReactiveViewController.swift
@@ -17,21 +17,22 @@ class ReactiveViewController: UIViewController, SetStatus, GestureReactorDelegat
   @IBOutlet weak var centerXConstraint: NSLayoutConstraint! //For updating the position of the box when dragging
   @IBOutlet weak var centerYConstraint: NSLayoutConstraint!
   
-  private var gestureReactor: GestureReactor = ReactiveGestureReactor(timerCreator: { interval in ReactiveTimerFactory.reactiveTimer(interval: interval) })
+  private var gestureReactor: GestureReactor!
   
   private let disposeBag = DisposeBag()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    gestureReactor.delegate = self
-
     let pan = UIPanGestureRecognizer(target: self, action: "handlePan:")
     pan.delegate = self
     let rotate = UIRotationGestureRecognizer(target: self, action: "handleRotate:")
     rotate.delegate = self
-    self.draggableView.gestureRecognizers = [pan, rotate]
+    draggableView.gestureRecognizers = [pan, rotate]
     
+    gestureReactor = ReactiveGestureReactor(timerCreator: { interval in ReactiveTimerFactory.reactiveTimer(interval: interval) }, gestureRecognizers: (pan, rotate))
+    gestureReactor.delegate = self
+
     
     ///
     ///
@@ -39,7 +40,7 @@ class ReactiveViewController: UIViewController, SetStatus, GestureReactorDelegat
     ///
     /// Uses custom infix on CGPoint to '-' or '+' two together.
     
-    let panLocation = pan.rx_event.map { $0.locationInView(self.view) - self.view.center }
+    let panLocation = pan.rx_event.map { [unowned self] in $0.locationInView(self.view) - self.view.center }
     panLocation.map { $0.x }
       .bindTo(self.centerXConstraint.rx_constant)
       .addDisposableTo(self.disposeBag)
